5d4cafaed888b6deaed0546b7306dd2c0f3259d0
Tabitha Lyn <tabithalyn6@gmail.com>
2023-07-17 20:47:45 -0400

    Style

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,23 @@ function App(data:CountryType) {
   const [countryData, setCountryData] = useState([data]);
   const [searchCountry, setSearchCountry] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [isSearched, setIsSearched] = useState(false);
+
+  // window.onload = () => {
+  //   const countryThing = document.querySelector(".country-data");
+  //   countryThing?.classList.add("hidden");
+  //   if (searchCountry.length !== 0) {
+  //     countryThing?.classList.remove("hidden");
+  //     countryThing?.classList.add("flex");
+  //   }
+  // }
 
   const countrySearch = (e: { key: string; }) => {
     if (e.key === "Enter" && searchCountry.length !== 0) {
       fetch(`https://restcountries.com/v3.1/name/${searchCountry}?fullText=true`)
       .then((res) => {
         if (res.ok) {
+          setErrorMessage("");
           return res.json();
         } else if (res.status === 404) {
           return Promise.reject(setErrorMessage("Country not found. Try again."));
@@ -18,18 +29,18 @@ function App(data:CountryType) {
       })
       .then((data) => {
         setCountryData(data);
-        console.log(data);
+        setIsSearched(true);
       })
     }
   }
 
   return (
-    <>
+    <div className="w-screen h-screen bg-gradient-to-tl from-purple-100 to-pink-100 flex items-center">
       <div className="w-full flex justify-center flex-wrap">
         <div className="flex w-full justify-center flex-wrap">
           <input
             type="text"
-            className="countryQuery w-3/4 m-3 p-3 border-2 border-neutral-200"
+            className="countryQuery xs:w-4/5 sm:w-3/5 md:w-2/5 m-3 p-3 border-2 border-purple-200 rounded-full"
             value={searchCountry}
             onChange={e => setSearchCountry(e.target.value)}
             onKeyDown={countrySearch}
@@ -38,38 +49,73 @@ function App(data:CountryType) {
             required
           />
           {errorMessage && (
-            <div className="w-3/5 p-4 bg-red-300 text-lg mb-4">{errorMessage}</div>
+            <div className="w-3/5 p-4 bg-[#fc5380] text-lg mb-4 rounded-lg">
+              {errorMessage}
+            </div>
           )}
         </div>
-        <div className="w-2/4 flex justify-center">
-        {countryData ? (
-            countryData.map((country:CountryType) => (
-              <div key="{country}" className="w-full flex flex-wrap">
+        {countryData && isSearched ? (
+        <div className="w-2/4 xs:w-4/5 justify-center country-data">
+            { countryData.map((country:CountryType) => (
+              <div key="{country}" className="w-full flex flex-wrap ">
                 <div className="flex flex-wrap w-full">
-                  <div className="w-1/2 flex justify-center place-items-center text-4xl py-2 bg-gray-400">{country.name?.common}</div>
-                  <div className="w-1/2 flex justify-center"><img src={country.flags?.svg.toString()} className="w-full" /></div>
+                  <div className="w-full flex justify-center place-items-center xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl py-2">
+                    <span className="bg-indigo-200 p-2 rounded-md text-white drop-shadow-lg shadow-sharp-indigo">{country.name?.common}</span>
+                    <img src={country.flags?.svg.toString()} className="xs:w-3/12 md:w-2/12 lg:w-2/12 p-4 rounded-md drop-shadow-lg" />
+                  </div>
                 </div>
 
-                <div className="w-1/2 p-4 flex flex-wrap justify-center bg-amber-300">
-                <div className="w-full pt-2 pb-1 tracking-wider font-light text-sm uppercase flex justify-center">Capital</div>{country.capital}</div>
-                <div className="w-1/2 p-4 flex flex-wrap justify-center bg-teal-400">
-                <div className="w-full pt-2 pb-1 tracking-wider font-light text-sm uppercase flex justify-center">Continent</div>{country.continents}</div>
+                <div className="w-1/2 xs:w-full p-4 flex flex-wrap justify-center">
+                  <div className="pt-2 pb-1 tracking-widest md:text-lg lg:text-xl uppercase flex justify-center">
+                    <span className="bg-purple-400 p-1 rounded-md text-pink-50 shadow-sharp-purple">Capital</span>
+                  </div>
+                  <span className="px-4 pt-1 text-lg flex items-center">{country.capital}</span>
+                </div>
 
-                <div className="w-1/2 p-4 flex flex-wrap justify-center bg-green-400">
-                <div className="w-full pt-2 pb-1 tracking-wider font-light text-sm uppercase flex justify-center">Population</div>{country.population?.toLocaleString()}</div>
-                <div className="w-1/2 py-4 pl-2 flex flex-wrap justify-center bg-rose-400">
-                <div className="w-full pt-2 pb-1 tracking-wider font-light text-sm uppercase flex justify-center">Currency</div>{Object.values(country.currencies || {}).map(({name}) => name).join(', ')}
-                <span className="ml-1">{Object.values(country.currencies || {}).map(({symbol}) => symbol)}</span>
-                <span className="ml-1">{Object.keys(country.currencies || {}).map((key) => key)}</span></div>
+                <div className="w-1/2 xs:w-full p-4 flex flex-wrap justify-center">
+                  <div className="pt-2 pb-1 tracking-widest md:text-lg lg:text-xl uppercase flex justify-center">
+                    <span className="bg-indigo-400 p-1 rounded-md text-pink-50 shadow-sharp-indigo2">Continent</span>
+                  </div>
+                  <span className="px-4 pt-1 text-lg flex items-center">{country.continents}</span>
+                </div>
+
+                <div className="w-1/2 xs:w-full p-4 flex flex-wrap justify-center">
+                  <div className="pt-2 pb-1 tracking-widest md:text-lg lg:text-xl uppercase flex justify-center">
+                    <span className="bg-pink-400 p-1 rounded-md text-pink-50 shadow-sharp-pink">Population</span>
+                  </div>
+                  <span className="px-4 pt-1 text-lg flex items-center">{country.population?.toLocaleString()}</span>
+                </div>
+
+                <div className="w-1/2 xs:w-full p-4 flex flex-wrap justify-center">
+                  <div className="pt-2 pb-1 tracking-widest md:text-lg lg:text-xl uppercase flex justify-center">
+                    <span className="bg-rose-400 p-1 rounded-md text-pink-50 shadow-sharp-rose">Currencies</span>
+                  </div>
+                  <span className="px-4 pt-1 text-lg flex items-center">
+                    {Object.values(country.currencies || {}).map(({name}) => name).join(', ')}
+                    <span className="px-2">&rarr;</span>
+                    <span className="ml-1">
+                      {Object.values(country.currencies || {}).map(({symbol}) => symbol)}
+                    </span>
+                    <span className="ml-1">
+                      {Object.keys(country.currencies || {}).map((key) => key)}
+                    </span>
+                  </span>
+                </div>
                 
-                <div className="w-full p-4 flex flex-wrap justify-center bg-amber-500">
-                <div className="w-full pt-2 pb-1 tracking-wider font-light text-sm uppercase flex justify-center">Languages</div>{Object.values(country.languages || {}).join(", ")}</div>
+                <div className="w-1/2 xs:w-full p-4 flex flex-wrap justify-center">
+                  <div className="pt-2 pb-1 tracking-widest md:text-lg lg:text-xl uppercase flex justify-center">
+                    <span className="bg-rose-300 p-1 rounded-md text-pink-50 shadow-sharp-rose2">Languages</span>
+                  </div>
+                  <span className="px-4 pt-1 text-lg flex items-center">
+                    {Object.values(country.languages || {}).join(", ")}
+                  </span>
+                </div>
               </div>
-            ))
-          ) : null}
-        </div>
+            ))}
+          </div>) : null
+          }
       </div>
-    </>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: [
     "./index.html",
@@ -8,7 +10,20 @@ export default {
     fontFamily: {
       'sans': ['Poppins', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'sharp-indigo': '5px 5px 0px 0px rgba(123, 142, 219)',
+        'sharp-purple': '5px 5px 0px 0px rgba(140, 79, 201)',
+        'sharp-indigo2': '5px 5px 0px 0px rgba(99, 110, 219)',
+        'sharp-pink': '5px 5px 0px 0px rgba(217, 82, 153)',
+        'sharp-rose': '5px 5px 0px 0px rgba(204, 65, 86)',
+        'sharp-rose2': '5px 5px 0px 0px rgba(214, 107, 120)'
+      }
+    },
+    screens: {
+      'xs': '375px',
+      ...defaultTheme.screens
+    }
   },
   plugins: [],
 }
